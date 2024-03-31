e81f50385813646dfe25ba7def15db522886d9df
yechanlee <firecare@naver.com>
2024-04-01 00:02:03 +0900

    그냥 동그라미임

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Center(
-        // child: Container( width: 50, height: 50, color: Colors.blue),
-        child: Image.asset('assets/salonde.png')
+         child: Container( width: 50, height: 50, color: Colors.blue),
+        // child: Image.asset('assets/salonde.png')
       )
     );
   }
